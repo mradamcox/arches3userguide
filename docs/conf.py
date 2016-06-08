1cb338510ea1906add1bf90e040a0516d72e8813
Adam Cox <mr.adamcox@gmail.com>
2016-06-08 09:18:24 -0600

    change theme to standard readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -46,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Arches v3 User Manual'
-copyright = u'2016, Getty Conservation Institute and World Monuments Fund'
 author = u'Legion GIS'
+copyright = u'2016, Getty Conservation Institute and World Monuments Fund'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -108,7 +109,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
